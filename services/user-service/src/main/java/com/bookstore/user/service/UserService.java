6951416fbe2d7586b224ef075247bb1055544f8f
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-04-24 11:13:29 +0530

    [Mrugan Kulkarni]: Password encryption implemented using BCrypt

--- services/user-service/src/main/java/com/bookstore/user/service/UserService.java
+++ services/user-service/src/main/java/com/bookstore/user/service/UserService.java
@@ -4,23 +4,28 @@ import com.bookstore.user.dto.UserRequestDTO;
 import com.bookstore.user.dto.UserResponseDTO;
 import com.bookstore.user.entity.User;
 import com.bookstore.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponseDTO register(UserRequestDTO request) {
 
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
         User user = new User(
                 request.getName(),
                 request.getEmail(),
-                request.getPassword()
+                encodedPassword
         );
 
         User saved = userRepository.save(user);
